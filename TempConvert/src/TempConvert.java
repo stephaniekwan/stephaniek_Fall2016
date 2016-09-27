3f160c234c2f8d902d867f007f04655ad1b13b63
stephaniekwan <steffy.kwan@gmail.com>
2016-09-27 08:31:51 -0700

    tempconvert 9/27

---
+++ TempConvert/src/TempConvert.java
@@ -0,0 +1,17 @@
+/* Name: Stephanie Kwan
+ * Date: 27 Sep 2016
+ * Program Name : TempConvert
+ * This program converts a temperature measurement from Fahrenheit
+ * to Celsius.
+ */
+
+public class TempConvert {
+
+	public static void main(String[] args) {
+		int fahrenheit = 32;
+		int celsius = (5/9)*(fahrenheit - 32);
+		System.out.println(celsius + "*C");
+
+	}
+
+}
